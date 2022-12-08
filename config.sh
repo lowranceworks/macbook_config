06b9d663e77b322335b67e842fd4cf46950216c4
Josh <lowranceworks@gmail.com>
2022-12-08 13:31:07 -0600

    feat: config.sh installs software with brew

--- config.sh
+++ config.sh
@@ -3,4 +3,10 @@
 cp ./files/home_directory/.zshrc $HOME
 cp -r ./files/home_directory/.oh-my-zsh/ $HOME
 
+# install terminal applications
+brew install $(<brew.txt)
+
+# install gui applications
+brew install --cask $(<brew-casks.txt)
+
 source $HOME/.zshrc
